b481d5313f0db4a92c182bd1fc2f35aa8d8df5f6
r2 <ahmadolukotun@gmail.com>
2025-08-21 08:17:14 +0100

    third commit

--- apps/saleoff/vite.config.ts
+++ apps/saleoff/vite.config.ts
@@ -1,9 +1,17 @@
+import { paraglideVitePlugin } from '@inlang/paraglide-js';
 import devtoolsJson from 'vite-plugin-devtools-json';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit(), devtoolsJson()],
+	plugins: [
+		sveltekit(),
+		devtoolsJson(),
+		paraglideVitePlugin({
+			project: './project.inlang',
+			outdir: './src/lib/paraglide'
+		})
+	],
 	test: {
 		expect: { requireAssertions: true },
 		projects: [
